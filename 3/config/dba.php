6581fcdabf2a7ee4a6faeb3a234c362e53ca1b7b
Алексей <mail@ashatrov.ru>
2025-02-11 23:19:51 +0300

    Done

--- 3/config/dba.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dbh = new \PDO('mysql:dbname=21302_', 'root', 'f2ox9erm');
-$dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-$dbh->query('SET NAMES utf8mb4;');
-
-return $dbh;
